333eeed78a4bb7817b9cca622c90cfcad1fb2a91
ianm1647 <ianmcg1128@gmail.com>
2025-04-01 14:31:18 -0500

    temporarily makes justdirethings breaker not break it

---
+++ src/main/java/ianm1647/bedrockores/integration/justdirethings/BlockBreakerMixin.java
@@ -0,0 +1,25 @@
+package ianm1647.bedrockores.integration.justdirethings;
+
+import com.direwolf20.justdirethings.common.blockentities.BlockBreakerT1BE;
+import ianm1647.bedrockores.common.block.BedrockOreBlock;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.state.BlockState;
+import net.neoforged.neoforge.common.util.FakePlayer;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(BlockBreakerT1BE.class)
+public abstract class BlockBreakerMixin {
+    @Inject(method = "mineBlock", at = @At("HEAD"))
+    public void mineBedrockOre(BlockPos pos, ItemStack tool, FakePlayer player, CallbackInfoReturnable<Boolean> cir) {
+        Level level = player.level();
+        BlockState state = level.getBlockState(pos);
+        if (state.getBlock() instanceof BedrockOreBlock) {
+            cir.cancel();
+        }
+    }
+}
